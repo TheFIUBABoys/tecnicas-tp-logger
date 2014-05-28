50b58d97fafe1693052c898a886d327cd0118106
lucasSimonelli <lucas.simonelli@fdvsolutions.com>
2014-05-28 17:47:38 -0300

    Changed base class from Throwable to Exception.

--- src/main/java/loggerExceptions/NotExistingLevelException.java
+++ src/main/java/loggerExceptions/NotExistingLevelException.java
@@ -3,5 +3,5 @@ package loggerExceptions;
 /**
  * Created by Tomas on 11/05/2014.
  */
-public class NotExistingLevelException extends Throwable {
+public class NotExistingLevelException extends Exception {
 }
